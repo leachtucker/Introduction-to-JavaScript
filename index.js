720c6e12b832dc31e97de3f52be1a933a350c38b
Tucker Leach <leachtucker@gmail.com>
2020-08-11 12:11:22 -0700

    Finished task 1

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
-
-
+let votingAge = 18;
+if (votingAge => 18) {
+    // Changed the conditional statement to reflect that the voting age (USA) is 18 *and older*
+    console.log(true);
+}
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
+let capacity = 'Under capacity';
+let occupancy = 10;
 
+if (occupancy > 25) {
+    capacity = 'Over capacity';
+}
 
-
+console.log(capacity);
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
-
-
+let num1 = Number("1999");
+console.log(num1);
 
 
 
 //Task d: Write a function to multiply a*b 
+function multiplyFunc(a, b) {
+    return a * b;
+}
 
-
-
+console.log(multiplyFunc(2, 5));
 
 
 /************************************************************** Task 2 **************************************************************/
@@ -48,7 +58,7 @@
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+
 
 
 
@@ -60,8 +70,8 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
@@ -72,7 +82,7 @@
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+
 
 
 
@@ -81,7 +91,7 @@
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+
 
 
 
@@ -94,10 +104,10 @@
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
-  
 
-  
-  
+
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
@@ -111,8 +121,3 @@
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
 //update your rock papers sissors code below to take a prompt from a user using the window object
-
-
-
-
-
